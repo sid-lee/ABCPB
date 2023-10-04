cb2b8a2faf88e58032fbdd1340b7ddbff55cb906
So-Eun Lee <slee42@myseneca.ca>
2023-10-04 18:11:13 -0400

    Copied books.js to backend under 'data' for test

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import books from './data/books.js';
 
 const port = 5001 ;
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('API is running...')
+});
+
+app.get('/api/books', (req, res) => {
+    res.json(books);
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
