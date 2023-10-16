52719a27f95eec5b070ddf9528f77f5798302141
So-Eun Lee <slee42@myseneca.ca>
2023-10-15 20:43:05 -0400

    cartSlice added

--- frontend/src/screens/BookScreen.jsx
+++ frontend/src/screens/BookScreen.jsx
@@ -1,21 +1,29 @@
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Form, Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap' ;
+import { useDispatch } from 'react-redux';
 import Rating from '../components/Rating';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { useGetBookDetailsQuery } from '../slices/booksApiSlice';
+import { addToCart } from '../slices/cartSlice';
 
 const BookScreen = () => {
 
     const { id: bookId } = useParams();
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
     const [ qty, setQty ] = useState(1);
 
     const { data: book, isLoading, error } = useGetBookDetailsQuery( bookId );
 
-    // console.log( [...Array(book.stockQty).keys()] );    
+    const addToCartHandler = () => {
+        dispatch(addToCart({ ...book, qty }));
+        navigate('cart');
+    };
 
     return ( 
         <>
@@ -75,7 +83,12 @@ const BookScreen = () => {
                                 )}
 
                                 <ListGroup.Item>
-                                    <Button className='btn-block' type='button' disabled={book.stockQty === 0}>
+                                    <Button 
+                                        className='btn-block' 
+                                        type='button' 
+                                        disabled={book.stockQty === 0}
+                                        onClick={addToCartHandler}
+                                    >
                                         Add To Cart
                                     </Button>
                                 </ListGroup.Item>
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,44 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { updateCart } from '../utils/cartUtils';
 
-const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : { cartItems: []};
-
-const addDecimals = (num) => {
-    return (Math.round(num *100) / 100 ).toFixed(2);
-}
+const initialState = localStorage.getItem('cart') 
+    ? JSON.parse(localStorage.getItem('cart')) 
+    : { cartItems: []};
 
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        addToCart: ( state, action) => {
+        addToCart: (state, action) => {
             const item = action.payload;
-
-            const existItem = state.cartItems.find((x)=> x._id === item._id);
-
-            if (existItem ){
-                state.cartItems = state.cartItems.map( (x) => x._id === existItem._id ? item : x); 
+      
+            const existItem = state.cartItems.find((x) => x._id === item._id);
+      
+            if (existItem) {
+              state.cartItems = state.cartItems.map((x) =>
+                x._id === existItem._id ? item : x
+              );
             } else {
-                state.cartItems = [ ...state.cartItems, item];
+              state.cartItems = [...state.cartItems, item];
             }
 
-            // Calculate items price
-            state.itemsPrice = addDecimals( state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0));
-            
-            // Calculate shipping price ( if it's over $100, shipping is free, or $10)
-            state.shippingPrice = addDecimals( state.itemsPrice > 100 ? 0: 10 ) ;
-            
-            // Calculate tax price (13%)
-            state.taxPrice = addDecimals( Number( (0.13 * state.itemsPrice)).toFixed(2) ) ;
-
-            // Calculate total
-            state.totalPrice = addDecimals(  
-                Number( state.itemsPrice) +
-                Number( state.shippingPrice) +
-                Number( state.taxPrice)
-            ).toFixed(2);
-
-            // store all cart info on the local storage
-            localStorage.setItem('cart',JSON.stringify(state));
+            return updateCart(state, item);
         },
     },
 });
