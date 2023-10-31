2877d4b1a55e6dada38f6853f1d724e6d86db947
So-Eun Lee <slee42@myseneca.ca>
2023-10-31 15:00:33 -0400

    Admin Menu, route added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -83,6 +83,21 @@ const Header = () => {
                         </LinkContainer>
                         ) }
 
+                        {/* Admin Links */}
+                        {userInfo && userInfo.isAdmin && (
+                            <NavDropdown title='Admin' id='adminmenu'>
+                            <LinkContainer to='/admin/productlist'>
+                                <NavDropdown.Item>Products</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to='/admin/orderlist'>
+                                <NavDropdown.Item>Orders</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to='/admin/userlist'>
+                                <NavDropdown.Item>Users</NavDropdown.Item>
+                            </LinkContainer>
+                            </NavDropdown>
+                        )}
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
